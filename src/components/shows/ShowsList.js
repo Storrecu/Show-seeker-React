eb40ac3cbfd98ba5279702c3f04a4c1aa673e3a0
Silvia <silviatc1993@gmail.com>
2023-12-29 11:43:33 +0100

    removed comented lines

--- src/components/shows/ShowsList.js
+++ src/components/shows/ShowsList.js
@@ -13,9 +13,6 @@ const ShowsList = ({ list, favShows, setFavShows }) => {
 
   console.log('lista de favoritos', favShows);
 
-  //se toma la lista que App le pasa por props y se ordena de forma alfabética. Una vez ordenado se almacena en una nueva variable y este será la lista que recibirá RenderList.
-  // const orderedShowList = [...list].sort();
-
   if (list.length === 0) {
     return <p>No matches found</p>;
   }
@@ -30,30 +27,3 @@ const ShowsList = ({ list, favShows, setFavShows }) => {
 };
 
 export default ShowsList;
-
-// const idShow = ev.target.id;
-// // console.log('list >>>>', list);
-// let foundShow = list.find((item) => item.id === idShow);
-// const indexFav = favShows.findIndex((item) => item.id === foundShow.id);
-
-// if (indexFav === -1) {
-//   setFavShows([...favShows, foundShow]);
-// } else {
-// const updatedFavShows = [...favShows];
-// updatedFavShows.splice(indexFav, 1);
-// setFavShows(updatedFavShows);
-// }
-
-// const handleClick = (ev) => {
-//   console.log('ev>>>>> ', ev);
-//   const idShow = ev.target.id;
-//   const foundShow = list.find((item) => item.id === idShow);
-//   const isAlreadyFav = favShows.some((favItem) => favItem.id === idShow);
-//   console.log('foundShow>>>>> ', foundShow);
-
-//   if (!isAlreadyFav) {
-//     setFavShows([...favShows, foundShow]);
-//   } else {
-//     setFavShows(favShows.filter((favItem) => favItem.id !== idShow));
-//   }
-// };
